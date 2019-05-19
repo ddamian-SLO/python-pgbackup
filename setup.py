152ab2eb6cce2dc69a195517137bd216ac59b780
ddamian-SLO <ddamian.sbp@gmail.com>
2019-05-19 19:27:42 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name = 'pgbackup',
+    version='0.1.0',
+    author='Davis Damian'
+    author_email='ddamian.sbp@gmail.com'
+    description='A utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ddamian-SLO/python-pgbackup',
+    packages=find_packages('src')
+)
